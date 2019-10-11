fac7d98818b15e544319cc04effc2a2092dabb74
Filippo Salmina <filippo.salmina@epfl.ch>
2019-10-11 21:46:55 +0200

    reoganization folder

--- BinanceData_tb.py
+++
@@ -1,9 +0,0 @@
-from BinanceData import BinanceData
-
-testRun = BinanceData('ETHUSDT', '15m', '2018.11.01 00:00:00', '2018.11.03 00:00:00')
-
-# get api data:
-pricesList = testRun.get_price_list()
-_time = testRun.get_time()
-
-print(pricesList['Open'].to_numpy(), _time)
