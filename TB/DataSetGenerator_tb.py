eced446fb708c17398aede4a2fb28a307d14e9d9
Filippo Salmina <filippo.salmina@epfl.ch>
2019-10-12 14:22:13 +0200

    1.DataSetGeneratr e TB--> implementation

--- TB/DataSetGenerator_tb.py
+++ TB/DataSetGenerator_tb.py
@@ -2,13 +2,44 @@
 from BinanceData import BinanceData
 from DataSetGenerator import DataSetGenerator
 
+import os
+
+import numpy as np
+import pandas as pd
+
 
 
 testRun = BinanceData('ETHUSDT',
                       '15m',
                        '2018.11.01 00:00:00',
-                       '2018.11.01 2:00:00')
+                       '2018.11.01 1:00:00')
 
-x = testRun.get_price_list()
 
-d = DataSetGenerator(x).write_data("test")
+'''
+matrix = np.random.randint(100, size=(3, 4))
+
+d = DataSetGenerator(matrix, ['a', 'b', 'c', 'd']).write_data("test")
+
+print(matrix)
+
+test = pd.read_csv("../CSVFile/test.csv")
+
+
+'''
+
+
+x = np.array([testRun.get_price_list()['Open'].to_numpy(), testRun.get_price_list()['Close'].to_numpy()])
+print(np.shape(x))
+
+
+d = DataSetGenerator(x, np.array(range(5))).write_data("test")
+
+t = pd.read_csv("../CSVFile/test.csv")
+
+print(t['0'])
+
+#x = np.concatenate((testRun.get_price_list()['Open'].to_numpy(),
+                    #testRun.get_price_list()['Close'].to_numpy()))
+
+
+
